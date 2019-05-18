c1d78a75b219895114ccd5dfc61a2389e3ce00e8
yangxuanhui <578986944@qq.com>
2019-05-18 16:52:53 +0800

    first try

---
+++ test_ci/wordladder/src/test/java/wordladder/ActuatorTest.java
@@ -0,0 +1,54 @@
+package wordladder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActuatorTest {
+
+
+    private MockMvc mockMvc; // 模拟MVC对象，通过MockMvcBuilders.webAppContextSetup(this.wac).build()初始化。
+    @Autowired
+    private WebApplicationContext wac; // 注入WebApplicationContext
+
+    @Before // 在测试开始前初始化工作
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testInfo() throws Exception{
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/actuator/info"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    public void testHealth() throws Exception{
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/actuator/health"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    public void testShutdown() throws Exception{
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/actuator/shutdown"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
