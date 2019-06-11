b9b2104fecdc26e003e63b0f220d36d3d5c8b485
yy <37563992+llIllIllIlllIll@users.noreply.github.com>
2019-06-11 08:49:24 +0800

    implement token

--- Feign/src/main/java/com/wordladder/feign/FeignController.java
+++ Feign/src/main/java/com/wordladder/feign/FeignController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
+import javax.servlet.http.*;
 import java.util.Enumeration;
 
 @RestController
@@ -15,15 +13,20 @@ public class FeignController {
     FeignService feignService;
 
     @RequestMapping("/get_wordladder")
-    public String ladder(@RequestParam(value="begin", defaultValue="code") String begin,@RequestParam(value="end", defaultValue="data") String end){
-        return feignService.ladder(begin,end);
-    }
-
-    @RequestMapping(value="/oauthtoken",method = RequestMethod.POST)
-    public String oauthToken(HttpServletRequest request){
-        String token = request.getParameter("OAUTH-TOKEN");
-        System.out.println(token);
-        feignService.getToken(token);
-        return token;
+    public String ladder(@RequestParam(value="begin", defaultValue="code") String begin, @RequestParam(value="end", defaultValue="data") String end,
+                         HttpServletRequest req, HttpServletResponse res) throws Exception{
+        Cookie[] cookies = req.getCookies();
+        boolean isLogin= false;
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("WLTOKEN"))
+                isLogin = true;
+        }
+        if(isLogin)
+            return feignService.ladder(begin,end);
+        else
+        {
+            res.sendRedirect("http://localhost:8083/home");
+            return "failed.";
+        }
     }
 }
--- wordladder/src/main/java/wordladder/LadderController.java
+++ wordladder/src/main/java/wordladder/LadderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -15,11 +16,6 @@ import javax.servlet.http.HttpSession;
 public class LadderController {
     @RequestMapping("/get_wordladder")
     public String ladder(@RequestParam(value="begin", defaultValue="code") String begin,@RequestParam(value="end", defaultValue="data") String end,HttpServletRequest req) {
-        HttpSession session = req.getSession();
-        Object token = session.getAttribute("token");
-        if(token==null){
-            return "未登录";
-        }
         Dict dict = new Dict();
         if(begin.equals("") || end.equals("")){
             return "input wrong";
